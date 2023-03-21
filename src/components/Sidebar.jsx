d7928d6174c320bd1022dfac249719a8ad6d6b51
Ali Hasham <60347508+ali-Git-cell@users.noreply.github.com>
2023-03-21 22:59:33 +0100

    sidebar completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,8 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
 const Sidebar = () => {
   const activeMenu = true;
+  const normalLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2"
+  const activeLink = "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2"
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (<>
@@ -18,7 +20,6 @@ const Sidebar = () => {
               <button
                 type="button"
                 onClick={() => {}}
-              
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -26,7 +27,23 @@ const Sidebar = () => {
             </TooltipComponent>
         </div>
         <div className="mt-10 ">
-          {}
+          {links.map(link => (
+          <div key={link.title}>
+            <p className="text-gray-400 m-3 mt-4 uppercase">{link.title}</p>
+            {link.links.map((item) => 
+              <NavLink
+              key={item.name}
+              to={`/${item.name}`}
+              onClick={()=> {}}
+              className={({isActive}) => 
+                isActive ?  activeLink : normalLink
+              }
+              >
+                {item.icon}
+                <span className='capitalize'>{item.name}</span>
+              </NavLink>
+            )}
+          </div>))}
         </div>
       </>)}
     </div>
